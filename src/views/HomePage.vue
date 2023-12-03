696db59a8170d11bf9c07207a6a6b4ead3fdeaf8
eraDong <qq1184434988@gmail.com>
2023-12-03 15:51:58 +0800

    2.static page done

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,51 @@
 import { ref } from 'vue'
 
 const searchContent = ref('')
+const centerDialogVisible = ref(false)
+const activeIndex = ref(0)
+const sortWay = ref('⬆️')
+
 // const objArr = ref('')
+const sortOptions = ref([
+  {
+    id: 0,
+    label: 'Deafult'
+  },
+  {
+    id: 1,
+    label: 'Amount'
+  },
+  {
+    id: 2,
+    label: 'Category'
+  },
+  {
+    id: 3,
+    label: 'Integrity'
+  }
+])
+
+const onEdit = () => {
+  console.log('edit')
+}
+
+const onDelete = () => {
+  console.log('delete')
+}
+
+const onCard = () => {
+  centerDialogVisible.value = true
+  // 申请描述内容
+}
+
+const onSort = (item) => {
+  if (sortWay.value === '⬆️' && activeIndex.value === item.id)
+    sortWay.value = '⬇️'
+  else sortWay.value = '⬆️'
+  activeIndex.value = item.id
+  // isUp.value = !isUp.value
+  // console.log(activeIndex.value)
+}
 </script>
 
 <template>
@@ -11,19 +55,66 @@ const searchContent = ref('')
       <el-aside>
         <el-input v-model="searchContent"></el-input>
         <el-button class="search">Search</el-button>
+        <div class="sort-list">
+          <div class="title">Sort according to</div>
+          <ul>
+            <!-- <li>
+              <el-button type="text">Default</el-button>
+            </li>
+            <li>
+              <el-button type="text">
+                Amount
+                <div class="up" v-if="isUp">⬆️</div>
+                <div class="down" v-else>⬇️</div>
+              </el-button>
+            </li>
+            <li>
+              <el-button type="text">
+                Category
+                <div class="up" v-if="isUp">⬆️</div>
+                <div class="down" v-else>⬇️</div>
+              </el-button>
+            </li>
+            <li>
+              <el-button type="text">
+                Integrity
+                <div class="up" v-if="isUp">⬆️</div>
+                <div class="down" v-else>⬇️</div>
+              </el-button>
+            </li> -->
+            <li
+              @click="onSort(item)"
+              :class="{ 'is-active': activeIndex === index }"
+              v-for="(item, index) in sortOptions"
+              :key="index"
+            >
+              <el-button type="text">
+                {{ item.label }}
+                <!-- <div class="up" v-if="isUp">⬆️</div>
+                <div class="down" v-else>⬇️</div> -->
+                <div
+                  v-if="activeIndex === index && activeIndex !== 0"
+                  v-html="sortWay"
+                ></div>
+              </el-button>
+            </li>
+          </ul>
+        </div>
       </el-aside>
       <el-main>
         <!-- 限制一个页面最多十个 不然页面会发生拉伸现象 -->
-        <el-card v-for="item in 10" :key="item">
+        <el-card v-for="item in 10" :key="item" @click="onCard">
           <div class="name">嘴部固定器</div>
-          <div class="image"><img src="@/assets/equipment1.png" alt="" /></div>
+          <div class="image">
+            <img @dragstart.prevent src="@/assets/equipment1.png" alt="" />
+          </div>
           <div class="id">ID : 1</div>
           <div class="count">Amount : 1</div>
-          <div class="department">Department : Jigsaw</div>
-          <div class="damaged">Integrity : 100%</div>
+          <div class="department">Category : Jigsaw</div>
+          <div class="damaged">Status : Avaliable</div>
           <div class="buttons">
-            <el-button type="text">edit</el-button>
-            <el-button type="text">delete</el-button>
+            <el-button type="text" @click.stop="onEdit">edit</el-button>
+            <el-button type="text" @click.stop="onDelete">delete</el-button>
           </div>
         </el-card>
         <el-pagination
@@ -36,10 +127,29 @@ const searchContent = ref('')
         />
       </el-main>
     </el-container>
+
+    <!-- describe of the equipment -->
+    <el-dialog
+      v-model="centerDialogVisible"
+      title="Description"
+      width="30%"
+      center
+    >
+      <span>
+        It should be noted that the content will not be aligned in center by
+        default
+      </span>
+      <template #footer>
+        <span class="dialog-footer"> </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <style lang="less" scoped>
+.is-active {
+  background-color: #253444;
+}
 .equipment {
   .el-aside {
     .el-input {
@@ -47,7 +157,7 @@ const searchContent = ref('')
       :deep(.el-input__wrapper) {
         background-color: transparent;
       }
-      :deep(.el-input__inner) {
+      :deep(.el-inpu t__inner) {
         color: #2980b9;
       }
     }
@@ -55,6 +165,32 @@ const searchContent = ref('')
       width: 60px;
       background-color: transparent;
       color: #2980b9;
+    }
+
+    .sort-list {
+      margin-top: 30px;
+      width: 215px;
+      background-color: #2c3e50;
+      border-radius: 5px;
+      padding: 10px;
+      .title {
+        color: #2980b9;
+        font-weight: 700;
+      }
+      ul {
+        text-align: center;
+        li {
+          display: flex;
+          margin-top: 15px;
+          border-radius: 5px;
+          .el-button {
+            width: 100%;
+          }
+          :hover {
+            background-color: #253444;
+          }
+        }
+      }
     }
   }
   .el-main {
@@ -74,7 +210,10 @@ const searchContent = ref('')
       border: none;
       color: #2980b9;
 
-      cursor: default;
+      cursor: pointer;
+      :hover {
+        background-color: #253444;
+      }
       .name {
         margin: 5px;
         font-weight: 700;
@@ -96,6 +235,17 @@ const searchContent = ref('')
         }
       }
     }
+  }
+
+  :deep(.el-dialog) {
+    .el-dialog__title {
+      font-weight: 700;
+      color: #2980b9;
+    }
+    .el-dialog__body {
+      color: #2980b9;
+    }
+    background-color: #2c3e50;
   }
 }
 </style>
--- src/views/ModPage.vue
+++ src/views/ModPage.vue
@@ -1,3 +1,150 @@
+<script setup>
+import { ref } from 'vue'
+
+const isMod = ref(false)
+</script>
+
 <template>
-  <div>123</div>
+  <el-container>
+    <el-aside>
+      <div class="mod-input">
+        <div class="title">Please input your mod key</div>
+        <div class="input"><el-input></el-input></div>
+      </div>
+    </el-aside>
+    <el-main>
+      <div class="title">ADD your equipment</div>
+      <div class="basic">
+        <div class="amount">Amount : <el-input></el-input></div>
+        <div class="category">Category : <el-input></el-input></div>
+        <div class="status">Status : <el-input></el-input></div>
+      </div>
+      <div class="descrip">
+        <div class="description">
+          Description :
+          <el-input type="textarea" :rows="4" resize="none"></el-input>
+        </div>
+      </div>
+      <div class="btn">
+        <el-button type="primary" round>Submit</el-button>
+        <div class="warn" v-if="isMod === false">
+          🔒 GET THE PERMISSION FIRST
+        </div>
+      </div>
+    </el-main>
+  </el-container>
 </template>
+
+<style lang="less" scoped>
+.el-container {
+  .el-aside {
+    .mod-input {
+      width: 230px;
+      background-color: #2c3e50;
+      padding: 20px;
+      border-radius: 5px;
+      .title {
+        color: #2980b9;
+        font-weight: 700;
+        margin-bottom: 10px;
+      }
+      .input {
+        .el-input {
+          width: 215px;
+          :deep(.el-input__wrapper) {
+            background-color: transparent;
+          }
+          :deep(.el-input__inner) {
+            color: #2980b9;
+          }
+        }
+      }
+    }
+  }
+
+  .el-main {
+    margin-right: 130px;
+    width: 100%;
+    background-color: #2c3e50;
+
+    color: #2980b9;
+    border-radius: 5px;
+    .title {
+      font-weight: 700;
+      margin-bottom: 30px;
+    }
+    .basic {
+      display: flex;
+      justify-content: space-between;
+      .amount {
+        .el-input {
+          width: 215px;
+          :deep(.el-input__wrapper) {
+            background-color: transparent;
+          }
+          :deep(.el-input__inner) {
+            color: #2980b9;
+          }
+        }
+      }
+      .category {
+        .el-input {
+          width: 215px;
+          :deep(.el-input__wrapper) {
+            background-color: transparent;
+          }
+          :deep(.el-input__inner) {
+            color: #2980b9;
+          }
+        }
+      }
+
+      .status {
+        .el-input {
+          width: 215px;
+          :deep(.el-input__wrapper) {
+            background-color: transparent;
+          }
+          :deep(.el-input__inner) {
+            color: #2980b9;
+          }
+        }
+      }
+
+      margin-bottom: 30px;
+    }
+
+    .descrip {
+      .description {
+        // .el-textarea {
+        //   width: 215px;
+        //   :deep(.el-textarea__wrapper) {
+        //     background-color: transparent;
+        //   }
+        //   :deep(.el-textarea__inner) {
+        //     color: #2980b9;
+        //   }
+        // }
+        :deep(.el-textarea__inner) {
+          background-color: transparent;
+          color: #2980b9;
+        }
+      }
+    }
+
+    .btn {
+      margin-top: 30px;
+      display: flex;
+      justify-content: space-between;
+      .el-button {
+        background-color: #2980b9;
+        color: #2c3e50;
+      }
+      .warn {
+        font-weight: 700;
+        color: #b92929;
+      }
+    }
+  }
+}
+</style>
